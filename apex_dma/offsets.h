03ec080a3930234348b03baa49026b921b331b67
KrackerCo <113476972+KrackerCo@users.noreply.github.com>
2023-10-05 09:52:20 -0400

    Updated Offsets
    Thank you helloworldxd, and Francisidk

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -1,17 +1,17 @@
 #define VERSION STEAM
 //Date 9/12/2023
-//GameVersion=v3.0.45.39
+//GameVersion=v3.0.46.46
 
 #if VERSION == STEAM
 
-#define OFFSET_ENTITYLIST 0x1e223f8 //cl_entitylist
-#define OFFSET_LOCAL_ENT 0x21d2738 //LocalPlayer might be moved to AVC_GameMovement
-#define OFFSET_NAME_LIST 0xc265a60 //NameList
+#define OFFSET_ENTITYLIST 0x1E213F8 //cl_entitylist
+#define OFFSET_LOCAL_ENT 0x21D1738 //LocalPlayer might be moved to AVC_GameMovement
+#define OFFSET_NAME_LIST 0xC264A60 //NameList
 
 
-#define OFFSET_LEVELNAME 0x16f61c0 //LevelName
-#define OFFSET_CLIENTSTATE 0x16f6000 //ClientState
-#define OFFSET_SIGNONSTATE 0x16f60ac //SignonState
+#define OFFSET_LEVELNAME 0x16f51c0 //LevelName
+#define OFFSET_CLIENTSTATE 0x16f5000 //ClientState
+#define OFFSET_SIGNONSTATE 0x16f50ac //SignonState
 
 
 #define OFFSET_TEAM 0x037c //m_iTeamNum
@@ -27,9 +27,9 @@
 
 #define OFFSET_WEAPON_NAME 0x1798 //m_weaponNameIndex 2nd one
 #define OFFSET_OFF_WEAPON 0x1964 //m_latestNonOffhandWeapons
-#define OFFSET_IN_ATTACK 0x07421868 // in_attack
-#define OFFSET_IN_TOGGLE_DUCK 0x07421888 //in_toggle_duck
-#define OFFSET_IN_ZOOM 0x074219e8 //in_zoom
+#define OFFSET_IN_ATTACK 0x07420848 // in_attack
+#define OFFSET_IN_TOGGLE_DUCK 0x07420888 //in_toggle_duck
+#define OFFSET_IN_ZOOM 0x074209d8 //in_zoom
 
 #define OFFSET_LIFE_STATE 0x06c8 //m_lifeState, >0 = dead
 #define OFFSET_BLEED_OUT_STATE 0x26a0 //m_bleedoutState, >0 = knocked
@@ -45,7 +45,7 @@
 #define OFFSET_OBSERVING_TARGET 0x3460 //m_hObserverTarget
 
 #define OFFSET_MATRIX 0x11a350 //ViewMatrix
-#define OFFSET_RENDER 0x74200a8 //ViewRender displays ESp, heath dist names etc
+#define OFFSET_RENDER 0x741F0A8 //ViewRender displays ESp, heath dist names etc
 
 #define OFFSET_WEAPON 0x1954 //m_latestPrimaryWeapons
 #define OFFSET_BULLET_SPEED 0x19c8 + 0x04d4 //CWeaponX!m_flProjectileSpeed maybe its WeaponSettings.projectile_launch_speed now
@@ -77,6 +77,8 @@
 #define GLOW_TYPE 0x29c // Script_Highlight_GetState + 4
 #define GLOW_COLOR 0x1D0+ 0x30 // Script_CopyHighlightState 15th mov
 #define GLOW_FADE 0x388+ 0x30 // Script_Highlight_GetCurrentInsideOpacity 3rd result of 3 offsets consecutive or first + 8
-#define HIGHLIGHT_SETTINGS 0xB5F8620 //?
+#define HIGHLIGHT_SETTINGS 0xB5F7620 //?
 #define HIGHLIGHT_TYPE_SIZE 0x28 //?
+#define OFFSET_CROSSHAIR_LAST 0x1A88 //CPlayer!lastCrosshairTargetTime
+#define OFFSET_CROSSHAIR_START 0x1A84 //CPlayer!crosshairTargetStartTime
 #endif
