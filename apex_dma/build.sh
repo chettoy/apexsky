833258e593bd2a966925dc13448dc7759b8e3f20
chettoy <chettoy@with.moe>
2023-11-05 16:52:25 +0800

    Use cmake

--- apex_dma/build.sh
+++ apex_dma/build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-export RUSTFLAGS="-Awarnings"
-cd memflow_lib/memflow-win32-ffi/
-if cargo build --release ; then
-    cd ../memflow-qemu-procfs
-
-    if cargo build --release --all-features ; then
-        cd ../../
-        make
-    else
-        echo "Error while building memflow-qemu-procfs"
-    fi
-
-else
-    echo "Error while building memflow-win32-ffi"
-fi
+mkdir -p build && cd build
+cmake .. && cmake --build .
