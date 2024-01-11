1d851191cb42afd16f1756b2484c841f092a7f5b
chettoy <chettoy@with.moe>
2024-01-12 01:01:16 +0800

    Implement smooth aiming using Advanced Disturbance Rejection for the aimbot feature.

--- apex_dma/apexsky/src/skyapex/aimbot_utils.rs
+++ apex_dma/apexsky/src/skyapex/aimbot_utils.rs
@@ -3,4 +3,6 @@ use super::Skyapex;
 #[skyapex_impl]
 pub trait AimbotUtils {
     fn triggerbot_threshold_fov(&mut self, zoom_fov: f32, distance: f32) -> f32;
+    fn aimbot_smooth_x(&mut self, target_ptr: i64, current: f32, delta: f32, smooth: f32) -> f32;
+    fn aimbot_smooth_y(&mut self, target_ptr: i64, current: f32, delta: f32, smooth: f32) -> f32;
 }
