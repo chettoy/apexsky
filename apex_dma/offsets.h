311df083fb27fd4755bb9f84cdacc0850f25d1a8
chettoy <chettoy@with.moe>
2024-04-07 16:49:43 +0800

    Update offsets and dependencies.

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -1,10 +1,10 @@
-// Date 3/27/2024
-// GameVersion=v3.0.62.29
+// Date 4/7/2024
+// GameVersion=v3.0.62.30
 
 #define OFFSET_ITEM_ID 0x1538           // item id?      //updated 2/14/2024
 #define OFFSET_M_CUSTOMSCRIPTINT 0x1538 // m_customScriptInt //updated 2/14/2024
 #define OFFSET_YAW                                                             \
-  0x221c - 0x8 // m_currentFramePlayer.m_ammoPoolCount//updated 2/14/2024 - 0x8
+  0x223c - 0x8 // m_currentFramePlayer.m_ammoPoolCount//updated 4/7/2024 - 0x8
 
 #define HIGHLIGHT_SETTINGS 0xBB24350 // HighlightSettings  // updated 3/27/2024
 #define OFFSET_GLOW_CONTEXT_ID 0x28c // updated 1/25/2024
