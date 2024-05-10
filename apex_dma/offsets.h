3e8701533f2a37f33377a887bf79e40d22dc2655
unknown <1064810202@qq.com>
2024-05-10 11:35:04 +0800

    sdf

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -2,11 +2,11 @@
 // GameVersion=v3.0.67.34
 
 #define OFFSET_ITEM_ID 0x1538           // item id?      //updated 2/14/2024
-#define OFFSET_M_CUSTOMSCRIPTINT 0x1558 // m_customScriptInt //updated 5/8/2024
+#define OFFSET_M_CUSTOMSCRIPTINT 0x1538 // m_customScriptInt //updated 5/8/2024
 #define OFFSET_YAW                                                             \
   0x223c - 0x8 // m_currentFramePlayer.m_ammoPoolCount//updated 4/7/2024 - 0x8
 
-#define HIGHLIGHT_SETTINGS 0xade39c0 // HighlightSettings  // updated 5/8/2024
+#define HIGHLIGHT_SETTINGS 0xade4bc0// HighlightSettings  // updated 5/8/2024
 #define OFFSET_GLOW_CONTEXT_ID 0x28c // updated 1/25/2024
 #define GLOW_VISIBLE_TYPE 0x26c      // updated 1/25/2024
 #define OFFSET_GLOW_FIX 0x268        // updated 1/25/2024
