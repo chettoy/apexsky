f96de8de32b70a807b290f2538fb4351cebc71d9
Takury <109140392+bigdragon46@users.noreply.github.com>
2024-06-26 13:53:33 +0800

    Update offsets.h

--- apex_dma/offsets.h
+++ apex_dma/offsets.h
@@ -1,15 +1,14 @@
-// Date 6/4/2024
-// GameVersion=v3.0.71.58
+// Date 6/26/2024
+// GameVersion=v3.0.72.12
 
-#define OFFSET_ITEM_ID 0x1558           // item id?      //updated 4/23/2024
-#define OFFSET_M_CUSTOMSCRIPTINT 0x1558 // m_customScriptInt //updated 2/14/2024
-#define OFFSET_YAW                                                             \
-  0x223c - 0x8 // m_currentFramePlayer.m_ammoPoolCount//updated 4/7/2024 - 0x8
+#define OFFSET_ITEM_ID 0x1568     // item id?                                   //updated 6/26/2024
+#define OFFSET_M_CUSTOMSCRIPTINT 0x1568 // m_customScriptInt                    //updated 6/26/2024
+#define OFFSET_YAW 0x224c - 0x8 // m_currentFramePlayer.m_ammoPoolCount- 0x8    //updated 6/26/2024
 
-#define HIGHLIGHT_SETTINGS 0xade5c40 // HighlightSettings  // updated 5/22/2024
-#define OFFSET_GLOW_CONTEXT_ID 0x28c // updated 1/25/2024
+#define HIGHLIGHT_SETTINGS 0xb0d8f70 // HighlightSettings                       //updated 6/26/2024
+#define OFFSET_GLOW_CONTEXT_ID 0x29c                                            //updated 6/26/2024
 #define GLOW_VISIBLE_TYPE 0x26c      // updated 1/25/2024
-#define OFFSET_GLOW_FIX 0x268        // updated 1/25/2024
+#define OFFSET_GLOW_FIX 0x278                                                   //updated 6/26/2024
 // Mode: HighlightSettings + 0x34 * Context + 0x0
 // Color: HighlightSettings + 0x34 * Context + 0x4
 
