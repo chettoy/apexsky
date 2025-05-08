da37d7719f4d45afe1e176791d43adc36b2f9c81
chettoy <chettoy@with.moe>
2025-05-08 23:01:20 +0800

    Update offsets to Game v3.0.1.21

--- apexsky/apex1_common/src/data.rs
+++ apexsky/apex1_common/src/data.rs
@@ -4,7 +4,7 @@ use named_constants::named_constants;
 use secrecy::SecretString;
 
 /*
- * GameVersion=3.0.2.14
+ * GameVersion=3.0.1.21
  */
 
 pub static GAME_VER_DX12_PROCESS_NAME: Lazy<SecretString> =
